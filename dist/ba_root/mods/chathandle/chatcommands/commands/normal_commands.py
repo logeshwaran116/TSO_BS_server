8016db50212f55998a2fd85e3ce06ac081ed6d99
Logeshwaran <pswaran79@gmail.com>
2026-08-07 11:19:07 +0530

    list fix test 2

--- dist/ba_root/mods/chathandle/chatcommands/commands/normal_commands.py
+++ dist/ba_root/mods/chathandle/chatcommands/commands/normal_commands.py
@@ -180,30 +180,33 @@ def pingall(clientid):
 
 def list(clientid):
     """Returns The List Of ALL Connected Players: PID, CID, Username, IGN"""
+    try:
+        p = u'{0:^6}{1:^12}{2:^18}{3:^18}'
+        seprator = '\n' + ('-' * 90)
 
-    p = u'{0:^6}{1:^12}{2:^18}{3:^18}'
-    seprator = '\n' + ('-' * 90)
+        listtext = [p.format('PID', 'CID', 'Username', 'IGN') + seprator]
 
-    listtext = [p.format('PID', 'CID', 'Username', 'IGN') + seprator]
+        pid = 0
+        for ros in reversed(list(bs.get_game_roster())):
+            if ros.get('client_id') == -1:
+                continue  # skip the internal BCS server/host pseudo-account
 
-    pid = 0
-    for ros in bs.get_game_roster():
-        if ros.get('client_id') == -1:
-            continue  # skip the internal BCS server/host pseudo-account
+            cid = ros.get('client_id', 'N/A')
+            username = ros.get('display_string') or 'N/A'
 
-        cid = ros.get('client_id', 'N/A')
-        username = ros.get('display_string') or 'N/A'
+            players = ros.get('players') or []
+            if players:
+                for player in players:
+                    ign = player.get('name_full', 'N/A')           
+                    listtext.append(p.format(pid, cid, username, ign))
+                    pid += 1
+            else:
+                listtext.append(p.format('N/A', cid, username, 'N/A'))
 
-        players = ros.get('players') or []
-        if players:
-            for player in players:
-                ign = player.get('name_full', 'N/A')           
-                listtext.append(p.format(pid, cid, username, ign))
-                pid += 1
-        else:
-            listtext.append(p.format('N/A', cid, username, 'N/A'))
-
-    send("\n".join(listtext), clientid)
+        send("\n".join(listtext), clientid)
+    except Exception:
+        import traceback
+        traceback.print_exc()
 
 
 def accountid_request(arguments, clientid, accountid):
